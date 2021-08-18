b5dcdea6e6378b5e6961ca70ccb385ef0eea1e8f
Charalampos Fanoulis <yo@fanoulis.dev>
2021-08-18 21:55:18 +0300

    fun stuff

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { config as readEnvVariables } from 'dotenv';
 import type { Response } from 'express';
 import { json, urlencoded } from 'express';
+import helmet from 'helmet';
 import { AppModule } from './app.module.js';
 import type { RawBodyRequest } from './types/Express';
-
 if (process.env.NODE_ENV !== 'prodcution') readEnvVariables();
 
 const app = await NestFactory.create(AppModule, {
 	bodyParser: false
 });
 
-/*
- * Add raw body property
- */
+app.use(helmet());
+
+// Add raw body, for Slack sig verification
 const rawBodyBuffer = (req: RawBodyRequest, _res: Response, buf: Buffer, encoding: BufferEncoding) => {
 	if (buf && buf.length) {
 		req.rawBody = buf.toString(encoding || 'utf8');
